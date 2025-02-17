0a920787fb953c2ff298387039b3c323e5c1ac13
saidaniamine12 <mohamedaminesaidani7@gmail.com>
2025-02-17 04:51:50 +0100

    feat: add AOS animations to page component for improved visual effects

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -16,6 +16,8 @@ export default function Page() {
             className="top-0 flex-row h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 w-full"
           >
             <div
+              data-aos="zoom-in-down"
+              data-aos-delay="1000"
               className="flex items-center gap-2 w-full"
               style={{
                 width: "100%",
